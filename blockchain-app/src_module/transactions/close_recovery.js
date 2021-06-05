80b5c6593f5dc9cc51f39eca26ce85d1dfefdadd
Aoi <ht.so.ik0102@gmail.com>
2021-06-05 13:38:16 +0700

    create close_recovery.js

---
+++ blockchain-app/src_module/transactions/close_recovery.js
@@ -0,0 +1,41 @@
+const { BaseAsset } = require("lisk-sdk");
+const { closeRecoverySchema } = require("../schemas");
+const { CLOSE_RECOVERY_ASSET_ID } = require("../constant");
+
+class CloseRecoveryAsset extends BaseAsset {
+    name = "closeRecovery";
+    id = CLOSE_RECOVERY_ASSET_ID;
+    schema = closeRecoverySchema;
+
+    async apply({
+        asset, 
+        transaction, 
+        stateStore,
+        reducerHandler, 
+    }) {
+        const lostAccount = await stateStore.account.get(transaction.senderAddress);
+        if (!lostAccount.srs.status.active) {
+            throw new Error(`No active recovery found for address ${lostAccount.address.toString('hex')}.`);
+        }
+        if (!lostAccount.srs.status.rescuer.equals(asset.rescuer)) {
+            throw new Error("Incorrect rescuer address");
+        }
+
+        await reducerHandler.invoke('token:credit', {
+            address: lostAccount.address, 
+            amount: lostAccount.srs.config.deposit,
+        });
+
+        // reset recovery status
+        lostAccount.srs.status.active = false;
+        lostAcccout.srs.status.rescuer = Buffer.from('');
+        lostAccount.srs.status.created = 0;
+        lostAccount.srs.status.deposit = BigInt('0');
+        lostAccount.srs.status.vouchList = [];
+        await stateStore.account.set(lostAccount.address, lostAccount);
+    }
+}
+
+module.exports = CloseRecoveryAsset;
+
+
