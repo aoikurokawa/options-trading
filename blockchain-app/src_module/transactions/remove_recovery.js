4acc15db51411eac0decb8b458b879fba8b0dd0a
Aoi <ht.so.ik0102@gmail.com>
2021-06-05 13:50:32 +0700

    remove_recovery.js

---
+++ blockchain-app/src_module/transactions/remove_recovery.js
@@ -0,0 +1,40 @@
+const { BaseAsset } = require("lisk-sdk");
+const { removeRecoverySchema } = require("../schemas");
+const { REMOVE_RECOVERY_ASSET_ID } = require("../constant");
+
+class RemoveRecoveryAsset extends BaseAsset {
+    name = "removeRecovery";
+    id = REMOVE_RECOVERY_ASSET_ID;
+    schema = removeRecoverySchema;
+
+    async apply({
+        transaction, 
+        stateStore, 
+        reducerHandler,
+    }) {
+        const lostAccount = await stateStore.account.get(transaction.senderAddress);
+
+        if (lostAccount.srs.config.friends.length === 0) {
+            throw Error("Account does not have a recovery configurations");
+        }
+
+        if (lostAccount.srs.status.active) {
+            throw Error("There is active recovery in progress. Please close the recovery to remove recovery configuration.");
+        }
+
+        // reset all the default values
+        lostAccount.srs.config.friends = [];
+        lostAccount.srs.config.recoveryThreshold = 0;
+        lostAccount.srs.config.delayPeriod = 0;
+        lostAccount.srs.config.deposit = BigInt('0');
+        lostAccount.srs.status.rescuser = Buffer.from('');
+        lostAccount.srs.status.deposit = BigInt('0');
+        lostAccount.srs.status.vouchList = [];
+        lostAccount.srs.status.created = 0;
+        lostAccount.srs.status.active = false;
+        await stateStore.account.set(lostAccount.address, lostAccount);
+    }
+}
+
+module.exports = RemoveRecoveryAsset;
+
