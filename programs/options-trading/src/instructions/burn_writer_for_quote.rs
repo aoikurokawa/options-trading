1526ed1a644a701440fd809cb00d56953f8381f8
Aoi <ht.so.ik0102@gmail.com>
2022-05-10 20:31:00 +0700

    Implement burn writer for quotes quote

--- programs/options-trading/src/instructions/burn_writer_for_quote.rs
+++ programs/options-trading/src/instructions/burn_writer_for_quote.rs
@@ -1,7 +1,7 @@
 use crate::errors;
 use crate::state::option_market::OptionMarket;
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount};
+use anchor_spl::token::*;
 // use solana_program::{program_error::ProgramError, system_program};
 
 #[derive(Accounts)]
@@ -21,7 +21,7 @@ pub struct BurnWriterForQuote<'info> {
 }
 
 impl<'info> BurnWriterForQuote<'info> {
-    fn accounts(ctx: &Context<BurnWriterForQuote>) -> Result<()> {
+    pub fn accounts(ctx: &Context<BurnWriterForQuote>) -> Result<()> {
         // Validate the Quote asset pool matches the OptionMarket
         if ctx.accounts.quote_asset_pool.key() != ctx.accounts.option_market.quote_asset_pool {
             return Err(errors::ErrorCode::QuotePoolAccountDoesNotMatchMarket.into());
@@ -36,7 +36,7 @@ impl<'info> BurnWriterForQuote<'info> {
     }
 
     // Validate there is enough quote assets in the pool
-    fn quotes_in_pool(ctx: &Context<BurnWriterForQuote>, size: u64) -> Result<()> {
+    pub fn quotes_in_pool(ctx: &Context<BurnWriterForQuote>, size: u64) -> Result<()> {
         if ctx.accounts.quote_asset_pool.amount
             < size
                 .checked_mul(ctx.accounts.option_market.quote_amount_per_contract)
@@ -47,4 +47,46 @@ impl<'info> BurnWriterForQuote<'info> {
 
         Ok(())
     }
+}
+
+pub fn handler(ctx: Context<BurnWriterForQuote>, size: u64) -> Result<()> {
+    let option_market = &ctx.accounts.option_market;
+    let seeds = &[
+        option_market.underlying_asset_mint.as_ref(),
+        option_market.quote_asset_mint.as_ref(),
+        &option_market.underlying_amount_per_contract.to_le_bytes(),
+        &option_market.quote_amount_per_contract.to_le_bytes(),
+        &option_market.expiration_unix_timestamp.to_le_bytes(),
+        &[option_market.bump_seed],
+    ];
+    let signer = &[&seeds[..]];
+
+    // Burn the size of WriterTokens
+    let cpi_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info().clone(),
+        Burn {
+            mint: ctx.accounts.writer_token_mint.to_account_info(),
+            from: ctx.accounts.writer_token_src.to_account_info(),
+            authority: ctx.accounts.user_authority.to_account_info(),
+        },
+        signer,
+    );
+    burn(cpi_ctx, size)?;
+
+    // Transfer the quote assets to the writer's account
+    let cpi_accounts = Transfer {
+        from: ctx.accounts.quote_asset_pool.to_account_info(),
+        to: ctx.accounts.writer_quote_dest.to_account_info(),
+        authority: ctx.accounts.option_market.to_account_info(),
+    };
+    let cpi_token_program = ctx.accounts.token_program.clone();
+    let cpi_ctx =
+        CpiContext::new_with_signer(cpi_token_program.to_account_info(), cpi_accounts, signer);
+    let quote_transfer_amount = option_market
+        .quote_amount_per_contract
+        .checked_mul(size)
+        .unwrap();
+    transfer(cpi_ctx, quote_transfer_amount)?;
+    
+    Ok(())
 }
