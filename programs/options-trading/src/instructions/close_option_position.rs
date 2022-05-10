fe604dc04a57be7aa2bb755a2bb0c6270f737a9b
Aoi <ht.so.ik0102@gmail.com>
2022-05-10 20:13:34 +0700

    Implement handler function in close option

--- programs/options-trading/src/instructions/close_option_position.rs
+++ programs/options-trading/src/instructions/close_option_position.rs
@@ -1,7 +1,7 @@
 use crate::errors;
 use crate::state::option_market::OptionMarket;
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount};
+use anchor_spl::token::*;
 // use solana_program::{program_error::ProgramError, system_program};
 
 #[derive(Accounts)]
@@ -25,7 +25,7 @@ pub struct CloseOptionPosition<'info> {
 }
 
 impl<'info> CloseOptionPosition<'info> {
-    fn accounts(ctx: &Context<CloseOptionPosition>) -> Result<()> {
+    pub fn accounts(ctx: &Context<CloseOptionPosition>) -> Result<()> {
         // Validate the WriterToken mint is the same as the OptionMarket
         if *ctx.accounts.writer_token_mint.to_account_info().key
             != ctx.accounts.option_market.writer_token_mint
@@ -49,4 +49,58 @@ impl<'info> CloseOptionPosition<'info> {
 
         Ok(())
     }
+}
+
+pub fn handler(ctx: Context<CloseOptionPosition>, size: u64) -> Result<()> {
+    let option_market = &ctx.accounts.option_market;
+    let seeds = &[
+        option_market.underlying_asset_mint.as_ref(),
+        option_market.quote_asset_mint.as_ref(),
+        &option_market.underlying_amount_per_contract.to_le_bytes(),
+        &option_market.quote_amount_per_contract.to_le_bytes(),
+        &option_market.expiration_unix_timestamp.to_le_bytes(),
+        &[option_market.bump_seed],
+    ];
+    let signer = &[&seeds[..]];
+
+    // Burn the size of WriterTokens
+    let cpi_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info().clone(),
+        Burn {
+            mint: ctx.accounts.writer_token_mint.to_account_info(),
+            from: ctx.accounts.writer_token_src.to_account_info(),
+            authority: ctx.accounts.user_authority.to_account_info(),
+        },
+        signer,
+    );
+    burn(cpi_ctx, size)?;
+
+    // Burn the Optiontokens
+    let cpi_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info().clone(),
+        Burn {
+            mint: ctx.accounts.option_token_mint.to_account_info(),
+            from: ctx.accounts.option_token_src.to_account_info(),
+            authority: ctx.accounts.user_authority.to_account_info(),
+        },
+        signer,
+    );
+    burn(cpi_ctx, size)?;
+
+    // Transfer the underlying assets from the pool to the destination
+    let cpi_accounts = Transfer {
+        from: ctx.accounts.underlying_asset_pool.to_account_info(),
+        to: ctx.accounts.underlying_asset_dest.to_account_info(),
+        authority: ctx.accounts.option_market.to_account_info(),
+    };
+    let cpi_token_program = ctx.accounts.token_program.clone();
+    let cpi_ctx =
+        CpiContext::new_with_signer(cpi_token_program.to_account_info(), cpi_accounts, signer);
+    let underlying_transfer_amount = option_market
+        .underlying_amount_per_contract
+        .checked_mul(size)
+        .unwrap();
+    transfer(cpi_ctx, underlying_transfer_amount)?;
+
+    Ok(())
 }
