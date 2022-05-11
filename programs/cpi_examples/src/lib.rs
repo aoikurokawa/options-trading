efcea49bbf2c5b578aa0c161bfb5bd54823fa86f
Aoi <ht.so.ik0102@gmail.com>
2022-05-11 11:03:21 +0700

    implement from

--- programs/cpi_examples/src/lib.rs
+++ programs/cpi_examples/src/lib.rs
@@ -1,10 +1,71 @@
 use anchor_lang::prelude::*;
+use anchor_lang::InstructionData;
+use anchor_spl::dex::serum_dex;
+use anchor_spl::dex::serum_dex::{
+    instruction::SelfTradeBehavior as SerumSelfTradeBehavior,
+    matching::{OrderType as SerumOrderType, Side as SerumSide},
+};
+use anchor_spl::dex::Dex;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
+use options_trading::instructions::{ExerciseOption, MintOptionV2};
+use options_trading::state::OptionMarket;
+use solana_program::msg;
+use std::num::NonZeroU64;
 
 pub mod errors;
 
 use errors as CpiExampleErrors;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[derive(Debug, AnchorSerialize, AnchorDeserialize)]
+pub enum SelfTradeBehavior {
+    DecrementTake = 0,
+    CancelProvide = 1,
+    AbortTransaction = 2,
+}
+
+impl From<SelfTradeBehavior> for SerumSelfTradeBehavior {
+    fn from(self_trade_behave: SelfTradeBehavior) -> SerumSelfTradeBehavior {
+        match self_trade_behave {
+            SelfTradeBehavior::DecrementTake => SerumSelfTradeBehavior::DecrementTake,
+            SelfTradeBehavior::CancelProvide => SerumSelfTradeBehavior::CancelProvide,
+            SelfTradeBehavior::AbortTransaction => SerumSelfTradeBehavior::AbortTransaction,
+        }
+    }
+}
+
+#[derive(Debug, AnchorSerialize, AnchorDeserialize)]
+pub enum OrderType {
+    Limit = 0,
+    ImmediateOrCancel = 1,
+    PostOnly = 2,
+}
+
+impl From<OrderType> for SerumOrderType {
+    fn from(order_type: OrderType) -> SerumOrderType {
+        match order_type {
+            OrderType::Limit => SerumOrderType::Limit,
+            OrderType::ImmediateOrCancel => SerumOrderType::ImmediateOrCancel,
+            OrderType::PostOnly => SerumOrderType::PostOnly,
+        }
+    }
+}
+
+#[derive(Debug, AnchorSerialize, AnchorDeserialize)]
+pub enum NewSide {
+    Bid,
+    Ask,
+}
+
+impl From<NewSide> for SerumSide {
+    fn from(side: NewSide) -> SerumSide {
+        match side {
+            NewSide::Bid => SerumSide::Bid,
+            NewSide::Ask => SerumSide::Ask,
+        }
+    }
+}
 
 #[program]
 pub mod cpi_examples {
