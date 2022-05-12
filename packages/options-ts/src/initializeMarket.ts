660dd86e2ddf4bfc0a4462852b6cf689a02d54c9
Aoi <ht.so.ik0102@gmail.com>
2022-05-12 20:07:32 +0700

    initialize market

---
+++ packages/options-ts/src/initializeMarket.ts
@@ -0,0 +1,43 @@
+import {
+  PublicKey,
+  Transaction,
+  SystemProgram,
+  Connection,
+  Keypair,
+} from "@solana/web3.js";
+import {
+  AccountLayout,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  MintLayout,
+  Token,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { FEE_OWNER_KEY } from "./fees";
+
+export const getOrAddAssociatedTokenAccountTx = async (
+  associatedAddress: PublicKey,
+  token: Token,
+  payer: PublicKey,
+  owner: PublicKey = FEE_OWNER_KEY
+) => {
+  try {
+    await token.getAccountInfo(associatedAddress);
+    return null;
+  } catch (err: any) {
+    if (
+      err.message === "Failed to find account" ||
+      err.message === "Invalid account owner"
+    ) {
+      return Token.createAssociatedTokenAccountInstruction(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        token.publicKey,
+        associatedAddress,
+        owner,
+        payer
+      );
+    } else {
+      err;
+    }
+  }
+};
